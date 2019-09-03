e7ce49dc4657b0a03dc293096f757b903ce59f7c
Edwin Mwaniki <edwinmwaniki@Edwins-MacBook-Pro.local>
2019-09-03 17:47:18 -0400

    Created gitignore file and configured SQLite Connection

--- src/main/java/com/ms3/codingAssignment/App.java
+++ src/main/java/com/ms3/codingAssignment/App.java
@@ -1,13 +1,34 @@
 package com.ms3.codingAssignment;
 
-/**
- * Hello world!
- *
- */
+import java.sql.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.Scanner;
+
 public class App 
 {
     public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    throws Exception {
+    	Class.forName("org.sqlite.JDBC");
+    	Connection c = null;    	
+        System.out.println( "Welcome to CSV TO SQL Program" );
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter file name without the file extension");
+        String fileName = scanner.next();
+    	try {
+        	c = DriverManager.getConnection("jdbc:sqlite:"+fileName+".db");
+        	System.out.println("SQLite DB connected");
+    	}catch(Exception e) {
+    		throw new Exception(e);
+    	}
+        File file = new File("files/input/"+fileName+".csv");
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        String st;
+        int count = 1;
+        while((st = br.readLine()) != null && count != 4 ) {
+        	System.out.println(st);
+        	count++;
+        }
     }
 }
